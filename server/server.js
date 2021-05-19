59919f4c330e25924c8393fdc8d9ff77c0e0e794
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-19 13:33:20 +0200

    created necessary files

--- server/server.js
+++ server/server.js
@@ -5,6 +5,23 @@ const app = express()
 
 app.use(express.json())
 
+async function run() {
+  try {
+    await mongoose.connect(
+      'connection string', 
+      { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true, 
+      }
+    )
+    console.log('Database is connected')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 app.listen(3001, () => {
   console.log('Server is running on port 3001')
 })
+
+// run()
