35ba2c02e66488eed16610b87e06ece58e79663b
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-19 15:49:34 +0200

    från handledning

---
+++ server/fromDavid.js
@@ -0,0 +1,17 @@
+/*
+
+- singular namn på namnen: router, controller, model
+- CRUD metod framför URL:en ist för tex /api/get...
+- filnamnen i resurs-mapparna: router, controller, model istället för user.router osv
+- I koddiagram --> kopplingar mellan tex order- och product-controller-filer för uppdatering av lagersaldo
+- speca schemat endast i ER-diagrammet, behövs ej i båda
+- sätt kort-egenskaperna färg, cmc, expansion osv som icke-required i product-schemat ------ pris, namn osv blir required i schemat
+- checkout är på front-end, varukorgen blir en order
+- Den riktiga produkten (ej bara _id:t) ligger i ordern
+- engelska i diagrammen
+- mitten-kolumnen ska vara namn i ER, tex 'products' ist för product[]
+- blanda ej stora och små bokstäver (var konsekvent)
+- order borde ha hela frakt-objektet{}, inte bara kopplas med _id:t
+- i ordern får produkten ett antal (kan defineras som antal i produkten som är inte required)
+
+*/
