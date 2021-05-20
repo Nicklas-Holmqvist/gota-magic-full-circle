3ee4188f88b3e55279b76b7457224769a7d73b5d
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-20 09:22:56 +0200

    tims changes

--- server/fromDavid.js
+++ server/fromDavid.js
@@ -1,6 +1,6 @@
 /*
 
-- singular namn på namnen: router, controller, model
+- ---CHECK--- - singular namn på namnen: router, controller, model
 - CRUD metod framför URL:en ist för tex /api/get...
 - filnamnen i resurs-mapparna: router, controller, model istället för user.router osv
 - I koddiagram --> kopplingar mellan tex order- och product-controller-filer för uppdatering av lagersaldo
@@ -13,5 +13,10 @@
 - blanda ej stora och små bokstäver (var konsekvent)
 - order borde ha hela frakt-objektet{}, inte bara kopplas med _id:t
 - i ordern får produkten ett antal (kan defineras som antal i produkten som är inte required)
+
+- Orderkontrollen kanske pratar med produktkontroller etc.
+- kommer vi behöva uppdatera flera modeller
+- Modeller kommer skapa modeller (+)
+- innehåller i ert schema kan defineras i ER-diagrammet
 
 */
