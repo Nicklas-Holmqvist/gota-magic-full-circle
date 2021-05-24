ac44c49cb89d65164f12f8e262d61e68da45a4e9
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-24 15:12:54 +0200

    able to find who is admin from all users

--- server/resources/auth/controller.js
+++ server/resources/auth/controller.js
@@ -2,17 +2,15 @@ const UserModel = require('../users/model')
 
 exports.auth = async (req, res, next) => {
   console.log('AUTH IS HERE =============')
-  // const user = await UserModel.findById('60aba162010c3c2f005643e8')
-  const admin = req.body.isAdmin
-  console.log(admin)
-  
-  if (admin) {
-    // res.send('User is an admin')
-    console.log('User is an admin')
-  } else {
-    // res.send('User is not an admin')
-    console.log('User is not an admin')
-  }
+  const users = await UserModel.find()
+
+  users.forEach((user) => {
+    if (user.isAdmin) {
+      console.log(user.email, 'is admin')
+    } else {
+      console.log(user.email, 'is NOT admin')
+    }
+  })
 
   next()
 }
