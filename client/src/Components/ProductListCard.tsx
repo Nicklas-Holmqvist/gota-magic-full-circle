457179259ee4063f65222b23cd77ef06c6718163
Olof-Wallgren <naturel.olof@hotmail.com>
2021-05-26 14:38:21 +0200

    updaterar stock på produckt

--- client/src/Components/ProductListCard.tsx
+++ client/src/Components/ProductListCard.tsx
@@ -13,6 +13,7 @@ import "../main.css";
 import { useProductContext } from "../Context/ProductContext";
 import { useCart } from "../Context/CartContext";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 interface Props {
   image: string;
   productname: string;
@@ -38,25 +39,27 @@ const useStyles = makeStyles({
   },
   centerBtnLeft: {
     fontSize: "0.8rem",
-    border: '1px solid grey',
-    background: '#FF7A2F',
+    border: "1px solid grey",
+    background: "#FF7A2F",
   },
   centerBtnRight: {
     fontSize: "0.8rem",
-    border: '1px solid grey',
-    borderTopLeftRadius: '0',
-    borderBottomLeftRadius: '0',
+    border: "1px solid grey",
+    borderTopLeftRadius: "0",
+    borderBottomLeftRadius: "0",
   },
   linkStyle: {
-    textDecoration: 'none'
-  }
+    textDecoration: "none",
+  },
 });
 
 function ProductListCard(props: Props) {
   const getProductId = useProductContext();
   const usecart = useCart();
   const style = useStyles();
-
+  const [dbStock, setDbStock] = useState(props.stock);
+  console.log(dbStock);
+  // updateStock(props.stock);
   return (
     <Grid container>
       <Grid item xs={12}>
@@ -66,27 +69,30 @@ function ProductListCard(props: Props) {
             <CardActions>
               <ButtonGroup>
                 <Button
-                  disabled={props.stock >= 0 ? false : true}
-                  onClick={() =>{
-                      if (props.stock > 0 || null) {
-                        usecart.addToCart(
-                          props.productname,
-                          props.price,
-                          props.image,
-                          props.id,
-                        )
-                      } else {
-                        alert('Inget i lager!')
-                        return                        
-                      }
+                  disabled={dbStock >= 1 ? false : true}
+                  onClick={() => {
+                    if (props.stock > 0 || null) {
+                      usecart.addToCart(
+                        props.productname,
+                        props.price,
+                        props.image,
+                        props.id
+                      );
+                    } else {
+                      alert("Inget i lager!");
+                      return;
                     }
-                  }
+                    updateStock(props.stock);
+                  }}
                   className={style.centerBtnLeft}
                 >
                   Köp
                 </Button>
 
-                <Link to={`/ProductPage/${props.id}`} className={style.linkStyle}>
+                <Link
+                  to={`/ProductPage/${props.id}`}
+                  className={style.linkStyle}
+                >
                   <Button
                     className={style.centerBtnRight}
                     onClick={() => {
@@ -100,12 +106,29 @@ function ProductListCard(props: Props) {
             </CardActions>
             <Typography className={style.font}>{props.productname}</Typography>
             <Typography className={style.font}>{props.price} kr</Typography>
-            <Typography className={style.font}>{props.stock >= 0 ? 'I Lager' : 'Ej i lager'}</Typography>
+            <Typography className={style.font}>
+              {dbStock >= 1 ? "I Lager" : "Ej i lager"}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
     </Grid>
   );
+  function updateStock(stock: number) {
+    let cartQuantity = 0;
+    const cartProduct = usecart.cart.filter((obj) => {
+      return props.id === obj.id;
+    });
+    cartProduct.forEach((obj) => {
+      cartQuantity = obj.quantity;
+    });
+    let newStock = stock - cartQuantity - 1;
+    setDbStock(newStock);
+    console.log(cartQuantity);
+  }
 }
 
 export default ProductListCard;
+function setDbStock(stock: any) {
+  throw new Error("Function not implemented.");
+}
