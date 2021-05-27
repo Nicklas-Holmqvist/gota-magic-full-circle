2a1883073dea66f9a94313d6c41f2fb783ebddad
Oliver Nygren <oliver.nygren@icloud.com>
2021-05-27 13:36:00 +0200

    restructuring for orders

--- client/src/Components/admin/Admin.tsx
+++ client/src/Components/admin/Admin.tsx
@@ -1,52 +1,20 @@
 import { Grid } from '@material-ui/core';
-import React from 'react'
+import React, { useState } from 'react'
+import Orders from './Orders';
 // import { useOrderContext, Order } from "../../Context/OrderContext";
 // import OrderList from './OrdeList'
 import Products from './Products'
 
 function Admin() {
+	const [displayProducts, setDisplayProducts] = useState(false)
+	const [displayOrders, setDisplayOrders] = useState(true)
 
-    // const importOrders = useOrderContext()
-
-    // const allOrders:Order[] = importOrders.allOrders
-    // console.log(allOrders)
-
-    // const viewAllOrders = allOrders.map((order) => (
-    //     <div key={order._id}>
-    //         <OrderList
-    //             orderNumber={order.orderNumber}
-    //             user={order.user} 
-    //             totalCost={order.totalCost}
-    //             shipping={order.shipping}
-    //             sent={order.sent}
-    //             products={order.products}
-    //         />
-    //     </div>
-    // ))
-
-    return(
-        <>
-        {/* <button style={{marginTop: "5rem"}} onClick={importOrders.fetchOrders}>Hämta order</button> */}
-        {/* <Grid container spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center" 
-            md={8}>
-            <Grid container alignContent='center'>
-                <Grid item xs={1}>Ordernr</Grid>
-                <Grid item xs={4}>Namn</Grid>  
-                <Grid item xs={2}>Fraktsätt</Grid>  
-                <Grid item xs={2}>Totalt värde</Grid>  
-                <Grid item xs={2}>Skickat</Grid>  
-            </Grid>
-            <Grid container direction='column'>
-            {viewAllOrders}
-            </Grid>
-        </Grid>   */}
-        <Products />
-
-        </>
-    )
+	return(
+		<>
+			<Products />
+			<Orders />
+		</>
+	)
 }
 
 export default Admin;
