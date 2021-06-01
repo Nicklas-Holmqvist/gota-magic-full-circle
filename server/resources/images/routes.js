6c11e941e50e0b94e4dbc69751317ec8ee671868
Timothy Gramnaes <Tgramnaes@gmail.com>
2021-06-01 11:39:32 +0200

    Working

--- server/resources/images/routes.js
+++ server/resources/images/routes.js
@@ -15,16 +15,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// router.use(express.static("uploads"));
-
-// router.post("/upload", fileUpload({ createParentPath: true }), (req, res) => {
-//   console.log(req.files);
-
-//   res.status(200).send();
-// });
 router.post("/uploads", upload.single("productImage"), controller.uploadImage);
-
-// router.post("/uploads", upload.single("productImage"), (req, res, next) => {
-//   console.log(req.file);
 
 module.exports = router;
